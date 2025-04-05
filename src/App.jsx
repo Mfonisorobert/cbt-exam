a5647a5d50f96cd8b476ce36c4d83b1bee062775
Mfonisorobert <mfonisorobert1@gmail.com>
2025-04-05 13:54:59 +0100

    cbt-exam

--- src/App.jsx
+++ src/App.jsx
@@ -174,7 +174,7 @@ if (!hasStarted) {
     )}
       <h3 className="text-2xl font-bold text-center text-blue-950 mb-2">
 
-      {showScore ? studentName : "YEAR 10 - CIVIC EDUCATION"}
+      {showScore ? studentName : "CIVIC EDUCATION"}
       </h3>
 
       
@@ -234,7 +234,7 @@ if (!hasStarted) {
 
 {/* This portion shows the result after submission */}
 
-{/* {showScore && (
+{showScore && (
   <div className="text-center mt-6">
     <h3 className="text-xl font-semibold mt-6 mb-2">Review:</h3>
     <div className="space-y-4 text-left mt-4">
@@ -252,7 +252,7 @@ if (!hasStarted) {
       })}
     </div>
   </div>
-)} */}
+)}
 
 
 </div>
