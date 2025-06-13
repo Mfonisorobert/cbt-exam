bf85813739723da17659daba905c8244489607e4
Mfonisorobert <mfonisorobert1@gmail.com>
2025-06-13 01:00:28 +0100

    randomizing questions and options

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import questionsData from "./data/questions.json";
 import Question from "./components/Question";
 import Timer from "./components/Timer";
-
+import { shuffleArray } from "./utils/shuffle";
 
 
 function App() {
@@ -15,20 +15,33 @@ function App() {
 const [studentName, setStudentName] = useState("");
 const [hasStarted, setHasStarted] = useState(false);
 
+// This is for shuffling of questions
+// Use this when you want to shuffle the questions
+useEffect(() => {
+  const randomized = shuffleArray(questionsData).map((q) => ({
+    ...q,
+    options: shuffleArray(q.options),
+  }));
+  setQuestions(randomized);
+}, []);
+
+// Shuffling of questions ends here
+
 
   // useEffect(() => {
   //   setQuestions(questionsData);
   // }, []);
 
-  // modified
-  useEffect(() => {
-    setQuestions(questionsData);
+  // Use this when you don't want to randomized the questions and options
+
+  // useEffect(() => {
+  //   setQuestions(questionsData);
   
-    const savedAnswers = localStorage.getItem("cbt-answers");
-    const savedIndex = localStorage.getItem("cbt-current-index");
-    if (savedAnswers) setAnswers(JSON.parse(savedAnswers));
-    if (savedIndex) setCurrentQIndex(Number(savedIndex));
-  }, []);
+  //   const savedAnswers = localStorage.getItem("cbt-answers");
+  //   const savedIndex = localStorage.getItem("cbt-current-index");
+  //   if (savedAnswers) setAnswers(JSON.parse(savedAnswers));
+  //   if (savedIndex) setCurrentQIndex(Number(savedIndex));
+  // }, []);
   // modified ends
 
   const handleOptionSelect = (option) => {
@@ -164,17 +177,37 @@ if (!hasStarted) {
     minHeight: '100vh',
   }}
 >
+
+  {/* Question Navigation Panel */}
+<div className="flex flex-wrap justify-center gap-2 mb-4">
+  {questions.map((_, idx) => {
+    const isAnswered = answers[idx] !== undefined;
+
+    return (
+      <button
+        key={idx}
+        onClick={() => setCurrentQIndex(idx)}
+        className={`w-8 h-8 square-full font-bold ${
+          isAnswered ? 'bg-green-900 text-white' : 'bg-red-800 text-white'
+        } ${currentQIndex === idx ? 'ring-2 ring-yellow-400' : ''}`}
+      >
+        {idx + 1}
+      </button>
+    );
+  })}
+</div>
+
   {/* Your CBT content here */}
 
 
-     <div className="max-w-2xl mx-auto mt-10 p-6 bg-gray-50 opacity-90 rounded-2xl shadow-xl">
+     <div className="max-w-2xl mx-auto mt-4 p-6 bg-gray-50 opacity-90 rounded-2xl shadow-xl">
 
      {!showScore && (
       <h1 className="text-3xl font-bold text-center text-blue-950 mb-2">SUN DREAM INT'L SCHOOL</h1>
     )}
       <h3 className="text-2xl font-bold text-center text-blue-950 mb-2">
 
-      {showScore ? studentName : "RESUMSSION"}
+      {showScore ? studentName : "COMPUTER STUDIES"}
       </h3>
 
       
@@ -186,7 +219,7 @@ if (!hasStarted) {
         <>
         <p className="text-1xl text-green-800 -mb-2  text-center"><span className="font-medium">{studentName}</span></p>
           
-          <Timer duration={1800} onTimeUp={handleTimeUp} /> {/* 5 minutes timer */}
+          <Timer duration={1500} onTimeUp={handleTimeUp} /> {/* 5 minutes timer */}
           {questions.length > 0 && (
             <Question
               questionObj={questions[currentQIndex]}
