28e875a9d8d9eac997a72a784dd60dae092e242f
Mfonisorobert <mfonisorobert1@gmail.com>
2025-06-25 15:54:51 +0100

    third term exams

--- src/App.jsx
+++ src/App.jsx
@@ -207,7 +207,7 @@ if (!hasStarted) {
     )}
       <h3 className="text-2xl font-bold text-center text-blue-950 mb-2">
       {/* <h2 className="text-2xl font-bold text-center text-blue-950 mb-2">THIRD TERM EXAM</h2> */}
-      {showScore ? studentName : "BIOLOGY - YEAR 11"}
+      {showScore ? studentName : "MARKETING - YEAR 10"}
       </h3>
 
       
